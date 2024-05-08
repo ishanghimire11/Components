80a8972fec6bab11da7e8945b210030e52893082
ishanghimire11 <ishanghimire11@gmail.com>
2024-05-08 22:54:03 +0545

    feat:  image fallback component

---
+++ image-fallback.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { cn } from "src/utils/cn";
+
+interface ImageLoaderProps {
+  imageUrl: string;
+  fallbackColor: string;
+  alt?: string;
+  className?: string;
+}
+
+const ImageLoader = ({
+  imageUrl,
+  fallbackColor,
+  alt,
+  className,
+}: ImageLoaderProps) => {
+  const [isImageLoaded, setIsImageLoading] = useState(false);
+
+  return (
+    <div
+      className="w-full h-full"
+      style={{ backgroundColor: isImageLoaded ? "transparent" : fallbackColor }}
+    >
+      <img
+        className={`w-full h-full ${isImageLoaded ? "block" : "hidden"} ${cn(
+          "w-full h-full",
+          className
+        )}`}
+        src={imageUrl}
+        onLoad={() => setIsImageLoading(true)}
+        alt={alt}
+      />
+    </div>
+  );
+};
+
+export default ImageLoader;
